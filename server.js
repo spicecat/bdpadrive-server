08c5cc40dd9de800d99e4d0114936b57bcc474a3
“spicecat” <ateh0701@gmail.com>
2022-06-28 17:20:31 -0500

    setup

---
+++ server.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose'),
+    app = require('./app')
+
+const PORT = 8000, CONNECTION_STRING = 'mongodb+srv://dbUser:dbPassword@cluster0.1wj2g.mongodb.net/test'
+
+mongoose.connect(CONNECTION_STRING, {}, err => {
+    if (err) console.log(err)
+    else app.listen(PORT, () => console.log('listening at port:', PORT))
